f02b2884c3ad939ba2d2f9102d20ac72826d1933
Javi ibanez <javier.ibanez.avilas@gmail.com>
2023-03-22 14:50:51 +0100

    Diseño final
    Naves 2D

---
+++ Assets/Script/BossTriggerBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class BossTriggerBox : MonoBehaviour 
+{
+	GameObject timeLine;
+	void Start()
+	{
+		if (GameObject.Find("TimeLine"))
+		{
+			timeLine = GameObject.Find("TimeLine");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		PlayableDirector pd = timeLine.GetComponent<PlayableDirector>();
+		pd.Play();
+	}
+}
---
+++ Assets/Script/GameStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats 
+{
+    public int liveLeft;
+    public string completed;
+    public int score;
+}
